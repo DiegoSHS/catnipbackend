17286d0c6d5343f0317febf7998a242c14126e2d
Diego López Ariza <diegoawesome2017@outlook.com>
2026-06-02 23:01:33 -0600

    keyword extends added to avoid type error

--- src/mongodb.ts
+++ src/mongodb.ts
@@ -1,4 +1,4 @@
-import { MongoClient, Db, Collection } from "mongodb"
+import { MongoClient, Db, Collection, type Document } from "mongodb"
 
 const MONGO_URI = process.env.MONGODB_URI ?? "mongodb://127.0.0.1:27017"
 const DEFAULT_DB_NAME = process.env.MONGODB_DB_NAME ?? "catnip"
@@ -31,7 +31,7 @@ export function getDb(): Db {
     return db
 }
 
-export async function getCollection<TSchema = any>(name: string): Promise<Collection<TSchema>> {
+export async function getCollection<TSchema extends Document>(name: string): Promise<Collection<TSchema>> {
     return (await connectMongo()).collection<TSchema>(name)
 }
 
